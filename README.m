90bd8c54a0b7e051521cf84340368c668fbd4314
ameelow <gombamillicent3@gmail.com>
2026-07-04 12:04:40 +0300

    Create README.md

---
+++ README.m
@@ -0,0 +1,17 @@
+# Patient Registration System (Java)
+
+This is a simple console-based Patient Registration System built using Java.
+
+## Features
+- Add patient details
+- View all registered patients
+- Simple menu-driven system
+
+## Technologies
+- Java
+- ArrayList
+- Scanner (input handling)
+
+## How to Run
+1. Compile: javac Main.java Patient.java
+2. Run: java Main
